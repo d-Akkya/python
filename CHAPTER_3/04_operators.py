70cea160134a8968f1083014d92caa669add6fcd
d-Akkya <akhileshbamane26@gmail.com>
2024-10-01 15:00:04 +0530

    assignment operators

--- CHAPTER_3/04_operators.py
+++ CHAPTER_3/04_operators.py
@@ -4,3 +4,10 @@ b = 4
 c = a + b   # +, -, *, /, %
 print(c)
 
+# Assignment Operators
+a = 4-2  #Assign 4-2 in a
+print(a)
+b = 6
+# b += 3  # Increment the value of b by 3 and then assign it to b
+b -= 3  # Decrement the value of b by 3 and then assign it to b
+print(b)
