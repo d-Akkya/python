ccc47141f0c2ae148b19deb3bd824560711d3d1c
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-10-01 10:19:51 +0000

    type conversion in input() function

--- CHAPTER_3/06_input.py
+++ CHAPTER_3/06_input.py
@@ -1,6 +1,6 @@
-a = input("Enter number 1: ") # input function collect the value as string
+a = int(input("Enter number 1: ")) # input function collect the value as string
 
-b = input("Enter number 2: ")
+b = int(input("Enter number 2: "))
 
 print("Number a is: ", a)
 print("Number b is: ", b)
