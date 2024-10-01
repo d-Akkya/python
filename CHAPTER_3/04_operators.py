3174e235a72a6925f5484e6236abb73d845d6cdb
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-10-01 09:37:45 +0000

    comparison operators

--- CHAPTER_3/04_operators.py
+++ CHAPTER_3/04_operators.py
@@ -11,3 +11,7 @@ b = 6
 # b += 3  # Increment the value of b by 3 and then assign it to b
 b -= 3  # Decrement the value of b by 3 and then assign it to b
 print(b)
+
+# Comparison Operators
+d = 5 != 7  #comparison operators always returns boolean value
+print(d)
