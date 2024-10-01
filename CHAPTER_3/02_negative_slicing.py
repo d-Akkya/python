997cf5841098521e4828a0ac12b312a14d3ec9ac
d-Akkya <akhileshbamane26@gmail.com>
2024-10-01 23:25:56 +0530

    slicing with skip value

--- CHAPTER_3/02_negative_slicing.py
+++ CHAPTER_3/02_negative_slicing.py
@@ -7,3 +7,7 @@ print(name[1:4])
 
 print(name[:4]) # is same as print(name[0:4])
 print(name[1:]) # is same as print(name[1:5]))
+
+# slicing with skip value
+a = "abcdefghijklmnopqrstuvwxyz"
+print(a[1:6:3])
