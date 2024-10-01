786e25ceb6d476ff52c8400e099184aa3be49067
d-Akkya <akhileshbamane26@gmail.com>
2024-10-01 14:39:09 +0530

    datatypes

---
+++ CHAPTER_3/02_datatypes.py
@@ -0,0 +1,9 @@
+a = 5   # a is an integer 
+
+b = 2.36  # b is a floating point number
+
+c = "akkya"   # c is a string
+
+d = False   # d is a boolean variable
+
+e = None   # e is a none type variable 
