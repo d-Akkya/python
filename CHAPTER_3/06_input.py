bf4655e7cda9a90c3e1f94c91e9a5f38ddac035f
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-10-01 10:19:06 +0000

    input() function

---
+++ CHAPTER_3/06_input.py
@@ -0,0 +1,7 @@
+a = input("Enter number 1: ") # input function collect the value as string
+
+b = input("Enter number 2: ")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum is ", a + b)
