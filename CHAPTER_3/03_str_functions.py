4332ca3d90d45db0179ea97de490f921d9c55751
d-Akkya <akhileshbamane26@gmail.com>
2024-10-03 13:50:29 +0530

    find()

--- CHAPTER_3/03_str_functions.py
+++ CHAPTER_3/03_str_functions.py
@@ -6,3 +6,5 @@ print(name.startswith("Ak"))
 print(name.capitalize())  # capitalize() – Capitalizes the first character of the string
 
 print(name.replace("good", "bad"))  # replace() - replace() – Replaces a substring with another string
+
+print(name.find("boy"))  # find() – Finds the first occurrence of a substring
