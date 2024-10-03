2b09a26616e3d36ecf7dfc8fcef9abfcde53d4af
d-Akkya <akhileshbamane26@gmail.com>
2024-10-03 13:48:43 +0530

    replace() method

--- CHAPTER_3/03_str_functions.py
+++ CHAPTER_3/03_str_functions.py
@@ -1,6 +1,8 @@
-name = "akkya is here"
+name = "akkya is a good good boy"
 
 print(len(name))
 print(name.endswith("ya"))
 print(name.startswith("Ak"))
 print(name.capitalize())  # capitalize() – Capitalizes the first character of the string
+
+print(name.replace("good", "bad"))  # replace() - replace() – Replaces a substring with another string
