7e84fc9f815fe3ec704378f825a2ee827550ea3e
d-Akkya <akhileshbamane26@gmail.com>
2024-10-03 13:34:38 +0530

    capitalize()

--- CHAPTER_3/03_str_functions.py
+++ CHAPTER_3/03_str_functions.py
@@ -1,5 +1,6 @@
-name = "Akkya"
+name = "akkya is here"
 
 print(len(name))
 print(name.endswith("ya"))
 print(name.startswith("Ak"))
+print(name.capitalize())  # capitalize() – Capitalizes the first character of the string
