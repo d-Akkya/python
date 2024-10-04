65b7919b324ad6a9e6d5cb7fd13f1ee1a9a5f623
d-Akkya <akhileshbamane26@gmail.com>
2024-10-04 17:16:19 +0530

    remove() method

--- CHAPTER_4/02_list_methods.py
+++ CHAPTER_4/02_list_methods.py
@@ -8,6 +8,6 @@ l1 = [1, 33, 64, 11, 70]
 # l1.sort()
 # l1.reverse()
 # l1.insert(3, 40023) #Insert 40023 such that its index in the list is 3
-value = l1.pop(3)
-print(value)
+# print(l1.pop(3))
+l1.remove(64)
 print(l1)
