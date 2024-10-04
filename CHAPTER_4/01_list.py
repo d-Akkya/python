1e66fcb4881fcc44ec97dc7a0e50671cec3f2132
d-Akkya <akhileshbamane26@gmail.com>
2024-10-04 16:21:05 +0530

    slice a list

--- CHAPTER_4/01_list.py
+++ CHAPTER_4/01_list.py
@@ -1,5 +1,9 @@
 friends = ["parshya", "rohya", 40024, 400.25, False, "Apple", "Orange"]
 
 print(friends[0])
+
 friends[0] = "akkya"
-print(friends[0])
+print(friends[0])  #Unlike strings lists are mutable 
+
+# slice a list
+print(friends[0:4])
