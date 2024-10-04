1b70afaee55fc4ebd21efa96b42071ae0da5ac6c
d-Akkya <akhileshbamane26@gmail.com>
2024-10-04 17:12:03 +0530

    pop()

--- CHAPTER_4/02_list_methods.py
+++ CHAPTER_4/02_list_methods.py
@@ -7,5 +7,7 @@ print(friends)
 l1 = [1, 33, 64, 11, 70]
 # l1.sort()
 # l1.reverse()
-l1.insert(3, 40023) #Insert 40023 such that its index in the list is 3
+# l1.insert(3, 40023) #Insert 40023 such that its index in the list is 3
+value = l1.pop(3)
+print(value)
 print(l1)
