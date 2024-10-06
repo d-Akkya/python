da303fe7705d7f3e18e85e69bc11f0f6aaaa4b4a
d-Akkya <akhileshbamane26@gmail.com>
2024-10-07 00:30:11 +0530

    if elif else ladder

--- CHAPTER_6/01_conditionals.py
+++ CHAPTER_6/01_conditionals.py
@@ -1,11 +1,15 @@
 a = int(input("Enter your age: "))
 
+# If elif else ladder
 if (a >= 18):
     print("You are above the age of concent")
     print("Good for you")
 
 elif(a<0):
-    print("You are entering an invalid age")
+    print("You are entering an invalid negative age")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
 
 else:
     print("You are below the age of concent")
