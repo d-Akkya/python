22f48810eb668c08d1bc882dfce0156b659581f4
d-Akkya <akhileshbamane26@gmail.com>
2024-10-06 20:06:55 +0530

    6th problem

---
+++ CHAPTER_5 - PS/Problem6.py
@@ -0,0 +1,20 @@
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter favourite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter favourite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter favourite language: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter favourite language: ")
+d.update({name: lang})
+
+print(d)
+
