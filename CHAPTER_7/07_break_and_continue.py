4267248936e34a64cfcb5c2c5bca3e38031e9abf
d-Akkya <akhileshbamane26@gmail.com>
2024-10-08 14:43:01 +0530

    continue keyword

--- CHAPTER_7/07_break_and_continue.py
+++ CHAPTER_7/07_break_and_continue.py
@@ -2,3 +2,8 @@ for i in range(100):
     if(i == 36):
         break #Exit the loop right now
     print(i)
+
+for i in range(31):
+    if(i == 20):
+        continue # Skip the iteration
+    print(i)
