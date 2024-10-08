ccee0ba64388d2a0f4aff139c686af3e9c6132b8
d-Akkya <akhileshbamane26@gmail.com>
2024-10-08 14:11:19 +0530

    for loop with lists, tuples, strings

---
+++ CHAPTER_7/05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+# For Loop with Lists
+l = [1, 4, 654, 34, 23, 56, 7, 65]
+
+for i in l:
+    print(i)
+
+# For Loop with Tuples
+t = (6, 45, 34, 67)
+for i in t:
+    print(i)
+
+# For Loop with Strings
+s = "Akkya"
+for i in s:
+    print(i)
