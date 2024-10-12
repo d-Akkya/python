8717b64ba85064aea57016adac602368f4f68149
d-Akkya <akhileshbamane26@gmail.com>
2024-10-12 14:14:25 +0530

    snake, water and gun game project

--- PROJECT 1/main.py
+++ PROJECT 1/main.py
@@ -8,8 +8,9 @@ import random
 computer = random.choice([1, -1, 0])
 youStr = input("Enter your choice: ")
 youDict = {"s": 1, "w": -1, "g": 0}
-you = youDict[youStr]
 reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+
+you = youDict[youStr]
 
 # By now we have 2 numbers (variables), you and computer
 print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
