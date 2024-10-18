53ca3899742ddd5b2b7aa5197fc8357434b4fbcb
d-Akkya <akhileshbamane26@gmail.com>
2024-10-18 23:24:38 +0530

    readline() with while loop

--- CHAPTER_9/03_more_file_functions.py
+++ CHAPTER_9/03_more_file_functions.py
@@ -3,19 +3,23 @@ f = open("F:/python/CHAPTER_9/file.txt")
 # lines = f.readlines()
 # print(lines, type(lines))
 
-line1 = f.readline()
-print(line1, type(line1))
+# line1 = f.readline()
+# print(line1, type(line1))
 
-line2 = f.readline()
-print(line2, type(line2))
+# line2 = f.readline()
+# print(line2, type(line2))
 
-line3 = f.readline()
-print(line3, type(line3))
+# line3 = f.readline()
+# print(line3, type(line3))
 
-line4 = f.readline()
-print(line4, type(line4))
+# line4 = f.readline()
+# print(line4, type(line4))
 
-line5 = f.readline()
-print(line5 == "")
+# line5 = f.readline()
+# print(line5 == "")
+line = f.readline()
+while(line != ""):
+    print(line)
+    line = f.readline()
 
 f.close()
