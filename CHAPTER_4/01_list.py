a57f7577c79a4c44207524214599cfafd4fefe7c
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-10-19 23:49:39 +0530

    Delete CHAPTER_4 directory

--- CHAPTER_4/01_list.py
+++
@@ -1,9 +0,0 @@
-friends = ["parshya", "rohya", 40024, 400.25, False, "Apple", "Orange"]
-
-print(friends[0])
-
-friends[0] = "akkya"
-print(friends[0])  #Unlike strings lists are mutable 
-
-# slice a list
-print(friends[0:4])
--- CHAPTER_4/03_tuple.py
+++
@@ -1,4 +0,0 @@
-a = (1, 23, 45, 343, False, "Rohit", "Prasad")
-
-print(a)
-print(type(a)) #Tuple is immutable like Strings
