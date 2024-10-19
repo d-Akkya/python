0663902f2bf52ae07fb9dc99d02f6e72300d3a99
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-10-19 23:49:15 +0530

    Delete CHAPTER_2 directory

--- CHAPTER_2/01_vairables.py
+++
@@ -1,9 +0,0 @@
-a = 1
-
-b = 2  #integer
-
-c = 7.22    #floating point numbers
-
-name = "Akkya"     #String
-
-print(a + b, name)
--- CHAPTER_2/06_input.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter number 1: ")) # input function collect the value as string
-
-b = int(input("Enter number 2: "))
-
-print("Number a is: ", a)
-print("Number b is: ", b)
-print("Sum is ", a + b)
