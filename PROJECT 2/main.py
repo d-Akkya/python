1a6a2087a7b5f46d16fc5ee1033363c28cc2331c
d-Akkya <akhileshbamane26@gmail.com>
2024-10-29 01:11:58 +0530

    guess the number game

---
+++ PROJECT 2/main.py
@@ -0,0 +1,19 @@
+import random
+
+n = random.randint(1, 100)
+a = -1
+guesses = 1
+
+while (a != n):
+    a = int(input('Guess the number: '))
+
+    if(a>n):
+        print('Lower number please!')
+        guesses += 1
+    elif(a<n):
+        print('Higher number please!')
+        guesses = guesses + 1
+    else:
+        print('Congratulations!!!')
+
+print(f'You have guessed the number {n} correctly in {guesses} attempts')
