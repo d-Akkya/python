106e10e56d8f0d1ce1019ea4f51e18051dcf1eed
d-Akkya <akhileshbamane26@gmail.com>
2024-11-05 00:11:06 +0530

    wake jarvis

--- Mega Project 1 - Jarvis/main.py
+++ Mega Project 1 - Jarvis/main.py
@@ -10,4 +10,25 @@ def speak(text):
     engine.runAndWait()
 
 if __name__ == "__main__":
-    speak('Hey sir how may i help you!')
+    speak('Initializing Jarvis....')
+    while True:
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if (word.lower() == "jarvis"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Listening...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+        except Exception as e:
+            print("Error; {0}".format(e))
