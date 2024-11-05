766358b24c6e23e8fc7066038d52bd09b89029f7
d-Akkya <akhileshbamane26@gmail.com>
2024-11-05 19:09:22 +0530

    play music with voice command

---
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=-EFA1EKWsBo",
+    "march": "https://www.youtube.com/watch?v=dONWrZoR0Fc",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "albatross": "https://www.youtube.com/watch?v=Bznxx12Ptl0"
+}
