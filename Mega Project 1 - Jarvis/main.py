89e3cdfc2b6dbc47e8067c05dbc240e2e7d4fbb0
d-Akkya <akhileshbamane26@gmail.com>
2024-11-05 18:37:15 +0530

    open google in browser by voice command

--- Mega Project 1 - Jarvis/main.py
+++ Mega Project 1 - Jarvis/main.py
@@ -9,6 +9,16 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com/in/")
+
 if __name__ == "__main__":
     speak('Initializing Jarvis....')
     while True:
@@ -26,9 +36,11 @@ if __name__ == "__main__":
                 speak("Ya")
                 # Listen for command
                 with sr.Microphone() as source:
-                    print("Listening...")
+                    print("Jarvis Active...")
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
-
+                    
+                    processCommand(command)
+                    
         except Exception as e:
             print("Error; {0}".format(e))
