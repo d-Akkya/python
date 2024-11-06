c82bcbdd2a19318e8cb37539ef75b2e130818a63
d-Akkya <akhileshbamane26@gmail.com>
2024-11-06 14:56:26 +0530

    change the assistant voice with gTTS

--- Mega Project 1 - Jarvis/main.py
+++ Mega Project 1 - Jarvis/main.py
@@ -7,13 +7,35 @@ import requests
 # from openai import OpenAI
 # from client import client
 from gtts import gTTS
+import pygame
+import os
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
-def speak(text):
+def speak_old(text):
     engine.say(text)
     engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3')
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load("temp.mp3")
+
+    # Play the music
+    pygame.mixer.music.play()
+
+    # Keep the program running so music can play fully
+    while pygame.mixer.music.get_busy():  # Check if music is playing
+        pygame.time.Clock().tick(10)  # Prevents high CPU usage in loop
+
+    # Unload the MP3 file
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3")
 
 # def aiProcess(command):
 #     client = client.api_key()
