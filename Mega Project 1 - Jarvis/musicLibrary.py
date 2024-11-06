3d7f3b20121cf500a13b238eba8b59cc23851674
d-Akkya <akhileshbamane26@gmail.com>
2024-11-06 12:19:35 +0530

    update songs

--- Mega Project 1 - Jarvis/musicLibrary.py
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -3,5 +3,6 @@ music = {
     "march": "https://www.youtube.com/watch?v=dONWrZoR0Fc",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
     "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
-    "albatross": "https://www.youtube.com/watch?v=Bznxx12Ptl0"
+    "albatross": "https://www.youtube.com/watch?v=Bznxx12Ptl0",
+    "punjabi": "https://www.youtube.com/watch?v=pk1LemEMs9w&list=PLgIeZ-48gPxiZlpJtGqEnP8xbyam4IMuN"
 }
